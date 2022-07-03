097fe8d7120a0eca7fdd9eda91d04c23e27ef0b8
cqxg <cqxg@yandex.ru>
2022-07-03 22:53:25 +0200

    fix: paths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://t6ej3rr6cl.execute-api.eu-west-1.amazonaws.com/products",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://umdpdv08kc.execute-api.eu-west-1.amazonaws.com",
+  import: "https://j8hxrf6c1g.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://t6ej3rr6cl.execute-api.eu-west-1.amazonaws.com/products",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
