82454bb7bfd8d4b0b6a8690ea2e4f1d83dd86a6c
cqxg <cqxg@yandex.ru>
2022-07-03 21:23:14 +0200

    fix: api path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,9 @@
-
 const API_PATHS = {
-  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://t6ej3rr6cl.execute-api.eu-west-1.amazonaws.com/products',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  product: "https://t6ej3rr6cl.execute-api.eu-west-1.amazonaws.com/products",
+  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://umdpdv08kc.execute-api.eu-west-1.amazonaws.com",
+  bff: "https://t6ej3rr6cl.execute-api.eu-west-1.amazonaws.com/products",
+  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
