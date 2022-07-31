64cc58d72befe1c6e711b9cd65a1524bc94a9cbe
cqxg <cqxg@yandex.ru>
2022-07-31 18:23:58 +0200

    feat: add eb link

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://j8hxrf6c1g.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://t6ej3rr6cl.execute-api.eu-west-1.amazonaws.com/products",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "http://cqxg-cart-api-dev.eu-west-1.elasticbeanstalk.com/api",
 };
 
 export default API_PATHS;
